3e328022ab5a20b58a565b3b91c1053c583a74b9
takumakoike <takuma.151e@gmail.com>
2025-01-05 20:38:32 +0900

    errer fix

--- line-reserve/src/app/api/route.ts
+++ line-reserve/src/app/api/route.ts
@@ -1,30 +1,35 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(){
-    const data = {message: "APIテスト成功", status:"OK"}
+export async function GET() {
+  const data = { message: "APIテスト成功", status: "OK" };
 
-    return NextResponse.json(data,{status: 200})
+  return NextResponse.json(data, { status: 200 });
 }
 
 export async function POST(request: Request) {
-    try{
+  try {
+    // const body = await NextResponse.json()
+    const body = await request.formData();
+    const name = body.get("name");
 
-        const body = await request.formData();
-        const name = body.get("name");
-        
-        const resOption = {
-            status: 200,
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }
-        
-        return new Response(JSON.stringify({message: `受信内容:${name}`}), resOption)
-    } catch(error){
-        return new Response(JSON.stringify({error: error.message}), {
-            status: 400,
-            headers: {"Content-Type": "application/json"}
-        })
-    }
-    
+    const resOption = {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    return new Response(
+      JSON.stringify({ message: `受信内容:${name}` }),
+      resOption
+    );
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error occurred";
+
+    return new Response(JSON.stringify({ error: errorMessage }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 }
