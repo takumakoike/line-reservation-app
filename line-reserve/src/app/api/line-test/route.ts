9916b2a7a35bdb3062a031d2e033a757b506b749
takumakoike <takuma.151e@gmail.com>
2025-01-06 23:31:49 +0900

    flexメッセージ修正

--- line-reserve/src/app/api/line-test/route.ts
+++ line-reserve/src/app/api/line-test/route.ts
@@ -8,6 +8,9 @@ export async function POST(request:Request) {
         const LINE_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN ? process.env.CHANNEL_ACCESS_TOKEN : process.env.NEXT_PUBLIC_CHANNEL_ACCESS_TOKEN;
         const LINE_API_PATH = "https://api.line.me/v2/bot/message/broadcast";
 
+        const today = new Date();
+        const tomorrow = new Date(today.getFullYear(), today.getMonth()+1, today.getDate()+1);
+
         const requestHeader = {
             "Content-Type": "application/json",
             Authorization: `Bearer ${LINE_ACCESS_TOKEN}`,
@@ -15,11 +18,49 @@ export async function POST(request:Request) {
 
         const requestBody = await JSON.stringify({
             messages: [
-            // メッセージ1件目
             {
-                type: "text",
-                text: "サンプルメッセージです",
+                type: "flex",
+                altText: "フレックスメッセージです",
+                contents: {
+                    type: "bubble",
+                    size: "giga",
+                    header: {
+                        type: "box",
+                        layout: "vertical",
+                        contents: [
+                            {
+                                type: "text",
+                                text: "予約希望日",
+                            },
+                        ]
+                    },
+                    body: {
+                        type: "box",
+                        layout: "vertical",
+                        contents: [
+                            {
+                                type: "text",
+                                text: `今日：${today}？それとも明日：${tomorrow}？`,
+                                size: "sm",
+                            }
+                        ]
+                    },
+                    footer : {
+                        type: "box",
+                        layout: "vertical",
+                        contents: [
+                            {
+                                type: "text",
+                                text: `${tomorrow}`
+                            }
+                        ]
+                    }
+                }
             },
+            // {
+            //     type:"text",
+            //     text: "ほげー"
+            // }
         ],
     });
 
